cf98b293d04f1de12bab213a62abfc2e1a4cd030
Victor Maslov <nakilon@gmail.com>
2021-05-23 01:44:09 +0300

    s/esoteric/esolangs

--- example_libera_logger/main.rb
+++ example_libera_logger/main.rb
@@ -1,3 +1,3 @@
 require "nakiircbot"
-NakiIRCBot.start "irc.libera.chat", "6666", "velik", "nakilon", "Libera.Chat Internet Relay Chat Network", "#esoteric",
+NakiIRCBot.start "irc.libera.chat", "6666", "velik", "nakilon", "Libera.Chat Internet Relay Chat Network", "#esolangs",
     password: File.read("password"), masterword: File.read("masterword") do |*| end
