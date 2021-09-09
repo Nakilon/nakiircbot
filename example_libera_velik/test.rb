b5a1ca018fe515f1bad4512805a0b445bc4f9613
Victor Maslov <nakilon@gmail.com>
2021-09-09 06:58:26 +0300

    tests should not be afraid to flood localhost

--- example_libera_velik/test.rb
+++ example_libera_velik/test.rb
@@ -1,22 +1,34 @@
 require "minitest/autorun"
+require "minitest/around/spec"  # we need Timeout because of blocking #gets
 
 require "socket"
 server = TCPServer.new 6666
 ENV["VELIK_NICKNAME"] = "velik2"
 ENV["VELIK_SERVER"] = "localhost"
-ENV["VELIK_CHANNEL"] = "##nakilon"
+# ENV["VELIK_CHANNEL"] = "##nakilon"
 Thread.new{ require_relative "main" }
+client = server.accept
+client.gets
+client.gets
 
-describe "velik" do
-  it do
-
-  client = server.accept
-  client.gets
-  client.gets
+describe "fast" do
+  around{ |test| Timeout.timeout(0.1){ test.call } }
+  it "ping" do
     client.puts ":user!user PRIVMSG #channel :ping"
     fail unless "PRIVMSG #channel :pong\n" == client.gets
+  end
+end
+describe "slow 3" do
+  around{ |test| Timeout.timeout(3){ test.call } }
+  it "[wiki ...]" do
     client.puts ":user!user PRIVMSG #channel :search with spaces [wiki bitwise cyclic tag], users [wiki user:nakilon], ignore dups [wiki user:nakilon], in text [wiki nakilon], weird chars [wiki created by Stack Exchange users]"
     fail unless "PRIVMSG #channel :https://esolangs.org/wiki/Bitwise%20Cyclic%20Tag https://esolangs.org/wiki/User:Nakilon https://esolangs.org/wiki/Velik https://esolangs.org/wiki/%3F%3F%3F\n" == client.gets
-
+  end
+end
+describe "slow 1" do
+  around{ |test| Timeout.timeout(1){ test.call } }
+  it "\\wiki" do
+    client.puts ":user!user PRIVMSG #channel :\\wiki asd"
+    fail unless "PRIVMSG #channel :ok asd\n" == p(client.gets)
   end
 end
--- lib/nakiircbot.rb
+++ lib/nakiircbot.rb
@@ -42,7 +42,7 @@ module NakiIRCBot
       loop do
         begin
           addr, msg = queue.shift
-          next unless addr && msg
+          next unless addr && msg   # TODO: how is it possible to have only one of them?
           addr = addr.codepoints.pack("U*")
           fail "I should not PRIVMSG myself" if addr == bot_name
           msg = msg.to_s.codepoints.pack("U*").chomp[/^(\x01*)(.*)/m,2].gsub("\x00", "[NUL]").gsub("\x0A", "[LF]").gsub("\x0D", "[CR]")
@@ -51,7 +51,7 @@ module NakiIRCBot
           prev_socket_time = prev_privmsg_time = Time.now
           socket_send.call privmsg
           break
-        end until queue.empty? if prev_privmsg_time + 5 < Time.now
+        end until queue.empty? if prev_privmsg_time + 5 < Time.now || server == "localhost"
 
         unless _ = Kernel::select([socket], nil, nil, 1)
           break if Time.now - prev_socket_time > 300
