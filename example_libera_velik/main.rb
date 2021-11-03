e2010a1ae3f5259da7daae3769e72416b586721c
Victor Maslov <nakilon@gmail.com>
2021-11-04 02:38:06 +0300

    deprecated the irc channel #esolangs controlled by shitty racist ops

--- example_libera_velik/main.rb
+++ example_libera_velik/main.rb
@@ -33,7 +33,7 @@ require "net/http"
 require "json/pure"
 
 NakiIRCBot.start (ENV["VELIK_SERVER"] || "irc.libera.chat"), "6666", nickname, "nakilon", "Libera.Chat Internet Relay Chat Network",
-    *(ENV["VELIK_CHANNEL"] || %w{ #esolangs ##nakilon #ruby-ru #ruby-offtopic #programming-ru }),
+    *(ENV["VELIK_CHANNEL"] || %w{ ##nakilon #ruby-ru #ruby-offtopic #programming-ru }),
     password: (File.read("password") if nickname == "velik"), masterword: File.read("masterword") do |str, add_to_queue|
 
   next unless /\A:(?<who>[^\s!]+)!\S+ PRIVMSG (?<dest>\S+) :(?<what>.+)/ =~ str
@@ -67,7 +67,7 @@ NakiIRCBot.start (ENV["VELIK_SERVER"] || "irc.libera.chat"), "6666", nickname, "
         help
       elsif $1 == "wiki" ; "\\wiki <Esolang wiki article or search query>"
       elsif $1 == "wp" ; "\\wp <Wikipedia article or search query>; \\wp-<lang> <query> (for <lang>.wikipedia.org)"
-      elsif $1 == "wa" ; "\\wa <Wolfram Alpha query>"
+      elsif $1 == "wa" ; "\\wa <Wolfram Alpha query>; \\was <query> (for shorter result)"
       else
         "unknown command #{$1.inspect}"
       end
@@ -264,6 +264,6 @@ NakiIRCBot.start (ENV["VELIK_SERVER"] || "irc.libera.chat"), "6666", nickname, "
           end
         end.join ", "
       ))
-    end unless dest == "#esolangs" || links.empty? || dup[/[a-z]/i]
+    end unless dest != "##nakilon" || links.empty? || dup[/[a-z]/i]
   end
 end
