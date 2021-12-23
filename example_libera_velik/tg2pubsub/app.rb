80610b4c09db16c743faf1dbfe23bd7cfc1defc6
Victor Maslov <nakilon@gmail.com>
2021-12-23 20:21:53 +0300

    newline in Logging; skipping empty text messages (such as just photos); closing the publisher isn't compatible with the fact the Function instances are reusable

--- example_libera_velik/tg2pubsub/app.rb
+++ example_libera_velik/tg2pubsub/app.rb
@@ -4,18 +4,14 @@ topic = Google::Cloud::Pubsub.new.topic "velik", skip_lookup: true
 
 FunctionsFramework.http do |request|
   s = JSON.load request.body.read
-  logger << s
+  logger << s << ?\n
 
   # -1001681138081 velik_test
-  if s.is_a?(Hash) && -1001172940616 == s.dig("message", "chat", "id")
+  if s.is_a?(Hash) && -1001172940616 == s.dig("message", "chat", "id") && s["message"]["text"]
     topic.publish_async JSON.dump( {
       addr: "#ruby-ru",
       msg: "[tg] #{s["message"]["from"]["username"]}: #{s["message"]["text"]}",
-    } ) do |result|
-      logger << result.succeeded?
-      logger << result
-    end
-    topic.async_publisher.stop.wait!
+    } ){ |result| logger << result.succeeded? << ?\n }
   end
 
   "OK"
