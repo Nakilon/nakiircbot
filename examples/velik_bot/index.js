55e393a30949f8bfe46fa3c34bb593e08db84eef
Victor Maslov <nakilon@gmail.com>
2023-10-23 22:48:26 +0300

    purple CSS

--- examples/velik_bot/index.js
+++ examples/velik_bot/index.js
@@ -7,17 +7,24 @@ Twitch.ext.onAuthorized( auth =>
     then( ([date,array]) => {
       document.getElementById("div").innerText = `last data update: ${date}`;
       const table = document.getElementById("table");
-      table.innerHTML = "<th>reputation</th>";
+      table.innerHTML = "<tr><th>reputation</th><th></th></tr><tr><td><hr noshade color='#f2eaff' size='2px' width='80%'></td><td><hr noshade color='#a970ff' size='2px' width='90%'></td></tr>";
       (function(_){
         array.toReversed().slice(0,5).forEach(([rating, who], i) => {
-          _(rating, who.join(", "));
+          _(rating, who);
         });
-        _("...", "");
+        _("...", []);
         _(array[0][0], array[0][1]);
       })( (a,b) => {
         const tr = document.createElement("tr");
         const tda = document.createElement("td"); tda.innerText = a; tr.append(tda); tda.style["text-align"] = "center";
-        const tdb = document.createElement("td"); tdb.innerText = b; tr.append(tdb);
+        const tdb = document.createElement("td");
+        b.forEach(who => {
+          const a = document.createElement("a");
+          a.href = `https://twitch.tv/${who}`;
+          a.append(who);
+          tdb.append(a);
+        } );
+        tr.append(tdb);
         table.append(tr);
       } )
     } )
